0617c52b861b4f398c35b4aa5e87180fd4e0b7f3
Hermonella <tovare13@gmail.com>
2017-02-21 15:31:41 +0100

    First upload

---
+++ Save Game.bat
@@ -0,0 +1,99 @@
+Echo off
+cls
+ECHO.
+ECHO ...............................................
+ECHO Wich game do you want to backup or restore?
+ECHO ...............................................
+ECHO.
+ECHO 1 - Prison Architect
+ECHO 2 - Rimworld
+ECHO 3 - Stardew Valley
+ECHO.
+Set /P M= Please type your option: 
+
+IF %M%==1 GOTO Prison Architect
+IF %M%==2 GOTO RimWorld
+IF %M%==3 GOTO Stardew Valley
+
+:Prison Architect
+
+cls
+ECHO.
+ECHO ...............................................
+ECHO What do you want to do?
+ECHO ...............................................
+ECHO.
+ECHO 1 - Backup The Save
+ECHO 2 - Restore The Save
+ECHO.
+Set /P M= Please type your option: 
+
+IF %M%==1 GOTO Backup
+IF %M%==2 GOTO Restore
+
+:Backup
+rmdir "./Prison Architect" /s /Q
+robocopy "%localappdata%\Introversion\Prison Architect" ".\Prison Architect" /s /e
+pause
+exit
+
+:Restore
+rmdir "%localappdata%\Introversion\Prison Architect" /s /Q
+robocopy ".\Saving" "%localappdata%\Introversion\Prison Architect" /s /e
+pause
+exit
+
+:Rimworld
+cls
+ECHO.
+ECHO ...............................................
+ECHO What do you want to do?
+ECHO ...............................................
+ECHO.
+ECHO 1 - Backup The Save
+ECHO 2 - Restore The Save
+ECHO.
+Set /P M= Please type your option: 
+
+IF %M%==1 GOTO Backup
+IF %M%==2 GOTO Restore
+
+:Backup
+rmdir "./RimWorld" /s /Q
+robocopy "%userprofile%\appdata\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios" ".\RimWorld" /s /e
+pause
+exit
+
+:Restore
+rmdir "%userprofile%\appdata\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios" /s /Q
+robocopy ".\RimWorld" "%userprofile%\appdata\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios" /s /e
+pause
+exit
+
+:Stardew Valley
+
+cls
+ECHO.
+ECHO ...............................................
+ECHO What do you want to do?
+ECHO ...............................................
+ECHO.
+ECHO 1 - Backup The Save
+ECHO 2 - Restore The Save
+ECHO.
+Set /P M= Please type your option: 
+
+IF %M%==1 GOTO Backup
+IF %M%==2 GOTO Restore
+
+:Backup
+rmdir "./Stardew Valley" /s /Q
+robocopy "%appdata%\StardewValley\Saves" ".\Stardew Valley" /s /e
+pause
+exit
+
+:Restore
+rmdir "%appdata%\StardewValley\Saves" /s /Q
+robocopy ".\Stardew Valley" "%appdata%\StardewValley\Saves" /s /e
+pause
+exit
