40170cec9ebe6454ea27edc7693bf840eeb3fc55
Hermonella <tovare13@gmail.com>
2017-02-23 10:52:55 +0100

    Added text about deleting folders.

--- Save Game.bat
+++ Save Game.bat
@@ -9,7 +9,7 @@ ECHO 1 - Prison Architect
 ECHO 2 - Rimworld
 ECHO 3 - Stardew Valley
 ECHO.
-Set /P M= Please type your option: 
+Set /P M= Please type your option:
 
 IF %M%==1 GOTO Prison Architect
 IF %M%==2 GOTO RimWorld
@@ -26,19 +26,21 @@ ECHO.
 ECHO 1 - Backup The Save
 ECHO 2 - Restore The Save
 ECHO.
-Set /P M= Please type your option: 
+Set /P M= Please type your option:
 
 IF %M%==1 GOTO Backup
 IF %M%==2 GOTO Restore
 
 :Backup
 rmdir "./Prison Architect" /s /Q
+ECHO Deleting backup folder
 robocopy "%localappdata%\Introversion\Prison Architect" ".\Prison Architect" /s /e
 pause
 exit
 
 :Restore
 rmdir "%localappdata%\Introversion\Prison Architect" /s /Q
+ECHO Deleting local folder
 robocopy ".\Saving" "%localappdata%\Introversion\Prison Architect" /s /e
 pause
 exit
@@ -53,19 +55,21 @@ ECHO.
 ECHO 1 - Backup The Save
 ECHO 2 - Restore The Save
 ECHO.
-Set /P M= Please type your option: 
+Set /P M= Please type your option:
 
 IF %M%==1 GOTO Backup
 IF %M%==2 GOTO Restore
 
 :Backup
 rmdir "./RimWorld" /s /Q
+ECHO Deleting backup folder
 robocopy "%userprofile%\appdata\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios" ".\RimWorld" /s /e
 pause
 exit
 
 :Restore
 rmdir "%userprofile%\appdata\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios" /s /Q
+ECHO Deleting local folder
 robocopy ".\RimWorld" "%userprofile%\appdata\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios" /s /e
 pause
 exit
@@ -81,19 +85,21 @@ ECHO.
 ECHO 1 - Backup The Save
 ECHO 2 - Restore The Save
 ECHO.
-Set /P M= Please type your option: 
+Set /P M= Please type your option:
 
 IF %M%==1 GOTO Backup
 IF %M%==2 GOTO Restore
 
 :Backup
 rmdir "./Stardew Valley" /s /Q
+ECHO Deleting backup folder
 robocopy "%appdata%\StardewValley\Saves" ".\Stardew Valley" /s /e
 pause
 exit
 
 :Restore
 rmdir "%appdata%\StardewValley\Saves" /s /Q
+ECHO Deleting local folder
 robocopy ".\Stardew Valley" "%appdata%\StardewValley\Saves" /s /e
 pause
 exit
